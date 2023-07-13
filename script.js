6410a1da2de8529cfd2ecebcaf235148e6ab1af1
SanLonch <93532736+SanLonch@users.noreply.github.com>
2023-07-13 21:59:51 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let number1 = Number(prompt("Введіть перше число:"));
+let number2 = Number(prompt("Введіть друге число:"));
+let sum = number1 + number2;
+let difference = number1 - number2;
+let product = number1 * number2;
+let division = number1 / number2;
+let result = " Сума: " + sum +   " Різниця: " + difference + " Добуток: " + product +   " Ділення: " + division;
+alert(result);
