b9aeff2a6c2007118186b2af22ed1ef509a60b23
SanLonch <93532736+SanLonch@users.noreply.github.com>
2023-08-10 21:20:15 +0300

    script.js

--- script.js
+++ script.js
@@ -1,86 +1,39 @@
-let a = 3;
-let b = 0;
-
-function isNumeric(value) {
-  return typeof value === 'number' && !isNaN(value);
-}
-
-function addNumbers(a, b) {
-  if (isNumeric(a) && isNumeric(b)) {
-    return a + b;
-  } else {
-    throw new Error ('Обидва аргументи повинні бути числами.');
-  }
-}
-
-function subtractNumbers(a, b) {
-  if (isNumeric(a) && isNumeric(b)) {
-    return a - b;
-  } else {
-    throw new Error ('Обидва аргументи повинні бути числами.');
-  }
-}
-
-function multiplyNumbers(a, b) {
-  if (isNumeric(a) && isNumeric(b)) {
-    return a * b;
-  } else {
-    throw new Error ('Обидва аргументи повинні бути числами.');
-  }
-}
-
-function divideNumbers(a, b) {
-  if (isNumeric(a) && isNumeric(b)) {
-    if (b !== 0) {
-      return a / b;
-    } else {
-      return('Ділення на нуль неможливе.');
+//1
+  function calculateAverage(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
     }
-  } else {
-    throw new Error ('Обидва аргументи повинні бути числами.');
+  
+    let avg = sum / arr.length;
+    return avg;
   }
-}
-console.log(addNumbers(a, b));     
-console.log(subtractNumbers(a, b));
-console.log(multiplyNumbers(a, b));
-console.log(divideNumbers(a, b)); 
-
-const numbers = [3, 8, 248, -10, -5, 209, 4];
-
-function findMin(arr) {
-  if (arr.length === 0) {
-    return undefined;
+  
+  let numbers = [1, 2, 3, 4, 5];
+  
+  let avg = calculateAverage(numbers);
+  console.log("The average is: " + avg);
+  
+//2
+  function User(name, age) {
+    this.name = name;
+    this.age = age;
   }
+  
+  User.prototype.sayHello = function () {
+    return `Hi. My name is ${this.name} and I'm ${this.age} years old`;
+  };
+  
+  const user = new User("Sherlock", 27);
+  console.log(user.sayHello());
+  
+//3
+const calculator = function (init = 1) {
+    return function () {
+        return init++;
+    };
+};
 
-  let min = arr[0];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-  }
-
-  return min;
-}
-
-function findMax(arr) {
-  if (arr.length === 0) {
-    return undefined;
-  }
-
-  let max = arr[0];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-
-  return max;
-}
-
-const minValue = findMin(numbers);
-const maxValue = findMax(numbers);
-
-console.log("Мінімальне число: " + minValue);
-console.log("Максимальне число: " + maxValue);
+const c = calculator();
+console.log(c());
+console.log(c());
