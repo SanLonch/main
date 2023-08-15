23a95eeb92a5a600a87bbb14bdfce32b932f33f5
SanLonch <93532736+SanLonch@users.noreply.github.com>
2023-08-15 22:46:18 +0300

    script.js

--- script.js
+++ script.js
@@ -1,86 +1,18 @@
-let a = 3;
-let b = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    const targetDate = new Date("2024-01-01T00:00:00");
 
-function isNumeric(value) {
-  return typeof value === 'number' && !isNaN(value);
-}
+    function updateCountdown() {
+        const currentDate = new Date();
+        const timeDifference = targetDate - currentDate;
 
-function addNumbers(a, b) {
-  if (isNumeric(a) && isNumeric(b)) {
-    return a + b;
-  } else {
-    throw new Error ('Обидва аргументи повинні бути числами.');
-  }
-}
+        const days = Math.floor(timeDifference / 1000 / 60 / 60 / 24);
+        const hours = Math.floor(timeDifference / 1000 / 60 / 60) % 24;
+        const minutes = Math.floor(timeDifference / 1000 / 60) % 60;
+        const seconds = Math.floor(timeDifference / 1000) % 60;
 
-function subtractNumbers(a, b) {
-  if (isNumeric(a) && isNumeric(b)) {
-    return a - b;
-  } else {
-    throw new Error ('Обидва аргументи повинні бути числами.');
-  }
-}
-
-function multiplyNumbers(a, b) {
-  if (isNumeric(a) && isNumeric(b)) {
-    return a * b;
-  } else {
-    throw new Error ('Обидва аргументи повинні бути числами.');
-  }
-}
-
-function divideNumbers(a, b) {
-  if (isNumeric(a) && isNumeric(b)) {
-    if (b !== 0) {
-      return a / b;
-    } else {
-      return('Ділення на нуль неможливе.');
+        const countdownElement = document.getElementById("countdown");
+        countdownElement.innerHTML = `До Нового Року залишилось: ${days} днів, ${hours} годин, ${minutes} хвилин, ${seconds} секунд`;
     }
-  } else {
-    throw new Error ('Обидва аргументи повинні бути числами.');
-  }
-}
-console.log(addNumbers(a, b));     
-console.log(subtractNumbers(a, b));
-console.log(multiplyNumbers(a, b));
-console.log(divideNumbers(a, b)); 
 
-const numbers = [3, 8, 248, -10, -5, 209, 4];
-
-function findMin(arr) {
-  if (arr.length === 0) {
-    return undefined;
-  }
-
-  let min = arr[0];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-  }
-
-  return min;
-}
-
-function findMax(arr) {
-  if (arr.length === 0) {
-    return undefined;
-  }
-
-  let max = arr[0];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-
-  return max;
-}
-
-const minValue = findMin(numbers);
-const maxValue = findMax(numbers);
-
-console.log("Мінімальне число: " + minValue);
-console.log("Максимальне число: " + maxValue);
+    setInterval(updateCountdown, 1000);
+});
